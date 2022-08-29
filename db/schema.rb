1d75e799446fd0ecf6e5226232041c6403579790
Joel Mast <joesmast@gmail.com>
2022-08-29 16:53:51 +0000

    I added create action to save to the database

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 20220827000150) do
 
   create_table "contacts", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "comments"
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
